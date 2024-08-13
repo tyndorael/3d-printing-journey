b939ea02597d01dad319425a95d53bb3580499ea
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2024-08-12 22:22:38 -0400

    add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  settings: { react: { version: '18.3' } },
+  extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked, ...tseslint.configs.stylisticTypeChecked],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
@@ -13,6 +15,7 @@ export default tseslint.config({
     globals: globals.browser,
   },
   plugins: {
+    react,
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
   },
@@ -22,5 +25,11 @@ export default tseslint.config({
       'warn',
       { allowConstantExport: true },
     ],
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules,
+  },
+  parserOptions: {
+    project: ['./tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: import.meta.dirname,
   },
 })
